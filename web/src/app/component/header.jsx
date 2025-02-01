802477fb61ac696cace3a640eedc8ee1753c2c8f
Sunny <lovesunday1006@gmail.com>
2025-02-02 00:52:27 +0700

    Update header.jsx

--- web/src/app/component/header.jsx
+++ web/src/app/component/header.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Header() {
   return (
-    <header>
-      <h1 className="text-2xl font-bold">Welcome to My Next.js App!</h1>
-      <p className="mt-2">This is a sample page with a navigation bar.</p>
+    <header className="center">
+      <h1 className="text-2xl font-bold">Welcome to My WebSite.</h1>
+      <p className="mt-2">This is Header</p>
     </header>
   );
 }
