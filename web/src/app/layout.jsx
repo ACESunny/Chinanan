baac93b3936325150d52d03663d67f93bfa59c30
Sunny <lovesunday1006@gmail.com>
2025-02-01 22:15:02 +0700

    update header and nav

--- web/src/app/layout.jsx
+++ web/src/app/layout.jsx
@@ -2,6 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
+import Header from "next/head";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +26,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header />
         {children}
         <SpeedInsights />
       </body>
