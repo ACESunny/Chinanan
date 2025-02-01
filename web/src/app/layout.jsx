9adbcba0e050186080e2a44b9a66835a777c21d3
Sunny <lovesunday1006@gmail.com>
2025-02-02 00:24:31 +0700

    update nav

--- web/src/app/layout.jsx
+++ web/src/app/layout.jsx
@@ -26,8 +26,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
         <Nav />
+        <Header />
         {children}
         <SpeedInsights />
       </body>
