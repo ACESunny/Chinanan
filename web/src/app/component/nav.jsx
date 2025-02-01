4a8e7508103783800bc12330697360ad11f2ac52
Sunny <lovesunday1006@gmail.com>
2025-02-01 22:22:44 +0700

    update Nav

---
+++ web/src/app/component/nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav>
+      <ul className="flex justify-center p-3">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
--- web/src/app/layout.jsx
+++ web/src/app/layout.jsx
@@ -2,8 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
-import Header from "next/head";
-
+import Header from "./component/header";
+import Nav from "./component/nav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +27,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
+        <Nav />
         {children}
         <SpeedInsights />
       </body>
