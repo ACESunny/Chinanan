15a28604cd6406264b9413ed44b9f966622edd18
Sunny <lovesunday1006@gmail.com>
2025-02-03 02:21:22 +0700

    update page os

---
+++ web/src/app/APIs/os/page.jsx
@@ -0,0 +1,7 @@
+export default function APIs() {
+    return (
+        <div>
+            <h1>This is my OS</h1>
+        </div>
+    );
+}
--- web/src/app/APIs/page.jsx
+++ web/src/app/APIs/page.jsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function APIs() {
     return (
         <div>
             <h1>This is my APIs</h1>
+            <Link href={/APIs/os}>Go to os</Link>
         </div>
     );
 }
