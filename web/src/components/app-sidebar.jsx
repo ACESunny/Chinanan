356cbde230d563e3024d2cfc6755eb54486f7c32
Sunny <lovesunday1006@gmail.com>
2025-02-03 09:51:15 +0700

    Update app-sidebar.jsx

--- web/src/components/app-sidebar.jsx
+++ web/src/components/app-sidebar.jsx
@@ -19,136 +19,37 @@ import {
 const data = {
   navMain: [
     {
-      title: "Getting Started",
-      url: "#",
-      items: [
-        {
-          title: "Installation",
-          url: "#",
-        },
-        {
-          title: "Project Structure",
-          url: "#",
-        },
-      ],
+      title: "Home",
+      url: "/",
+      // items: [
+      //   {
+      //     title: "Installation",
+      //     url: "#",
+      //   },
+      //   {
+      //     title: "Project Structure",
+      //     url: "#",
+      //   },
+      // ],
     },
     {
-      title: "Building Your Application",
-      url: "#",
-      items: [
-        {
-          title: "Routing",
-          url: "#",
-        },
-        {
-          title: "Data Fetching",
-          url: "#",
-          isActive: true,
-        },
-        {
-          title: "Rendering",
-          url: "#",
-        },
-        {
-          title: "Caching",
-          url: "#",
-        },
-        {
-          title: "Styling",
-          url: "#",
-        },
-        {
-          title: "Optimizing",
-          url: "#",
-        },
-        {
-          title: "Configuring",
-          url: "#",
-        },
-        {
-          title: "Testing",
-          url: "#",
-        },
-        {
-          title: "Authentication",
-          url: "#",
-        },
-        {
-          title: "Deploying",
-          url: "#",
-        },
-        {
-          title: "Upgrading",
-          url: "#",
-        },
-        {
-          title: "Examples",
-          url: "#",
-        },
-      ],
+      title: "APIs",
+      url: "/APIs",
     },
     {
-      title: "API Reference",
-      url: "#",
+      title: "Contact",
+      url: "/Contact",
       items: [
         {
-          title: "Components",
+          title: "Facebook",
           url: "#",
         },
         {
-          title: "File Conventions",
+          title: "Youtube",
           url: "#",
         },
         {
-          title: "Functions",
-          url: "#",
-        },
-        {
-          title: "next.config.js Options",
-          url: "#",
-        },
-        {
-          title: "CLI",
-          url: "#",
-        },
-        {
-          title: "Edge Runtime",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Architecture",
-      url: "#",
-      items: [
-        {
-          title: "Accessibility",
-          url: "#",
-        },
-        {
-          title: "Fast Refresh",
-          url: "#",
-        },
-        {
-          title: "Next.js Compiler",
-          url: "#",
-        },
-        {
-          title: "Supported Browsers",
-          url: "#",
-        },
-        {
-          title: "Turbopack",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Community",
-      url: "#",
-      items: [
-        {
-          title: "Contribution Guide",
+          title: "Email",
           url: "#",
         },
       ],
@@ -171,8 +72,8 @@ export function AppSidebar({
                   <GalleryVerticalEnd className="size-4" />
                 </div>
                 <div className="flex flex-col gap-0.5 leading-none">
-                  <span className="font-semibold">Documentation</span>
-                  <span className="">v1.0.0</span>
+                  <span className="font-semibold">Navigation Menu</span>
+                  <span className="">In complete 5%</span>
                 </div>
               </a>
             </SidebarMenuButton>
